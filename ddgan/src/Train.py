292b38f6de3daf3a470563a93080dd36b0b6e1ae
acse-jat20 <72030772+acse-jat20@users.noreply.github.com>
2021-06-05 17:44:06 +0100

    Starting transfer into an OOP format

--- ddgan/src/Train.py
+++ ddgan/src/Train.py
@@ -7,6 +7,7 @@ __all__ = []
 import tensorflow as tf
 import keras
 import numpy as np
+import sklearn
 
 
 class GAN:
@@ -16,54 +17,83 @@ class GAN:
 
     def __init__(self):
         """
-        Stuff happens here
+        Docstring
         """
 
+        # Keyword argument definitions
         self.kwargs = None
         self.nsteps = None
         self.ndims = None
         self.lmbda = None
         self.n_critic = None
-        self.batch_size = None # 32  
-        self.batches = None    # 900 
+        self.batch_size = None  # 32  
+        self.batches = None     # 900 
 
+        # Method definitions
         self.generator = tf.keras.Sequential()
         self.discriminator = tf.keras.Sequential()
+        self.generator_opt = tf.keras.optimizers.Adam(learning_rate=0.0001, beta_1=0, beta_2=0.9)
+        self.discriminator_opt = tf.keras.optimizers.Adam(learning_rate=0.0001, beta_1=0, beta_2=0.9)
+        self.g_loss = tf.keras.metrics.Mean('g_loss', dtype=tf.float32)
+        self.d_loss = tf.keras.metrics.Mean('d_loss', dtype=tf.float32)
+        self.w_loss = tf.keras.metrics.Mean('w_loss', dtype=tf.float32)
 
 
+        # Other definitions
+        self.g_summary_writer = None
+        self.d_summary_writer = None
+        self.w_summary_writer  = None
+            
     def setup(self, kwargs) -> None:
+        """
+        Setting up the neccecary values for the GAN class
+
+        Args:
+            kwargs (dict): key-value pairs of some stuff
+        """
         self.kwargs = kwargs
         self.nsteps = kwargs.pop("nsteps", 5)
         self.ndims = kwargs.pop("ndims", 5)
         self.lmbda = kwargs.pop("lambda", 10)
         self.n_critic = kwargs.pop("n_critic", 5)
-        self.batch_size = kwargs.pop("batch_size", 20) # 32  
-        self.batches = kwargs.pop("batches", 10) # 900
+        self.batch_size = kwargs.pop("batch_size", 20)  # 32  
+        self.batches = kwargs.pop("batches", 10)  # 900
 
         self.make_generator()
         self.make_discriminator()
 
 
-    def make_generator(self): #nsteps):
+    def make_logs(self):
+        """
+        Making logs
+        """
+        current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+        g_log_dir = './logs/gradient_tape/' + current_time + '/g'
+        d_log_dir = './logs/gradient_tape/' + current_time + '/d'
+        w_log_dir = './logs/gradient_tape/' + current_time + '/w'
+
+        self.g_summary_writer = tf.summary.create_file_writer(g_log_dir)
+        self.d_summary_writer = tf.summary.create_file_writer(d_log_dir)
+        self.w_summary_writer = tf.summary.create_file_writer(w_log_dir)
+
+    def make_generator(self):  # nsteps):
         """
         Generator
         """
-        self.generator.add(tf.keras.layers.Dense(5, input_shape=(5,), activation='relu')) # 5
+        self.generator.add(tf.keras.layers.Dense(5, input_shape=(5,), activation='relu'))   	# 5
         self.generator.add(tf.keras.layers.BatchNormalization())
-        self.generator.add(tf.keras.layers.Dense(10, activation='relu'))                  # 10
+        self.generator.add(tf.keras.layers.Dense(10, activation='relu'))                        # 10
         self.generator.add(tf.keras.layers.BatchNormalization())
-        self.generator.add(tf.keras.layers.Dense((5*self.nsteps), activation='relu'))     # 25
+        self.generator.add(tf.keras.layers.Dense((5*self.nsteps), activation='relu'))           # 25
         self.generator.add(tf.keras.layers.BatchNormalization())
-        self.generator.add(tf.keras.layers.Dense((5*nsteps), activation='tanh'))          # 25
+        self.generator.add(tf.keras.layers.Dense((5*self.nsteps), activation='tanh'))                # 25
         return None
 
-
-    def make_discriminator(self): #nsteps):
+    def make_discriminator(self):  # nsteps):
         """
         Discriminator
         """
-
-        self.discriminator.add(tf.keras.layers.Dense(5*self.nsteps, input_shape=(5*nsteps,)))
+        self.discriminator.add(tf.keras.layers.Dense(5*self.nsteps, input_shape=(5*self.nsteps,)))
         self.discriminator.add(tf.keras.layers.LeakyReLU(alpha=0.2))
         self.discriminator.add(tf.keras.layers.Dropout(0.3))
         self.discriminator.add(tf.keras.layers.Dense(10))
@@ -75,7 +105,6 @@ class GAN:
         self.discriminator.add(tf.keras.layers.Dense(1))
         return None
 
-
     def discriminator_loss(self, d_real, d_fake):
         d_loss = tf.reduce_mean(d_fake) - tf.reduce_mean(d_real)
         return d_loss
@@ -84,111 +113,104 @@ class GAN:
         g_loss = -tf.reduce_mean(d_fake)
         return g_loss
 
-
-@tf.function
-def train_step(noise, real, lmbda, n_critic, g, c, g_opt, c_opt, g1_loss, d1_loss, w_loss):   
-    batch_size = len(real)
-
-    for i in range(n_critic):    
+    def update_discriminator_loss(self, d_loss, batch_size, fake, real):
         with tf.GradientTape() as t:
-            with tf.GradientTape() as t1:
-                fake = g(noise, training=True) # training=False?
-                d_real = c(real, training=True)
-                d_fake = c(fake, training=True)
-                d_loss = discriminator_loss(d_real, d_fake)   #initial c loss
+            epsilon = tf.random.uniform(shape=[batch_size, 1], minval=0., maxval=1.)
+            interpolated = real + epsilon * (fake - real)
+            t.watch(interpolated)
+            c_inter = self.discriminator(interpolated, training=True)
+
+        grad_interpolated = t.gradient(c_inter, interpolated)
+        slopes = tf.sqrt(tf.reduce_sum(tf.square(grad_interpolated) + 1e-12, axis=[1]))
+        gradient_penalty = tf.reduce_mean((slopes - 1.) ** 2)
+
+        return d_loss + (self.lmbda*gradient_penalty)
+
+    def save_gan(self, epoch):
+        saved_g1_dir = './saved_g_' + str(epoch + 1)
+        saved_d1_dir = './saved_c_' + str(epoch + 1)
+        tf.keras.models.save_model(self.generator, saved_g1_dir)
+        tf.keras.models.save_model(self.discriminator, saved_d1_dir)
+
+    def write_summary(self, epoch):        
+        with self.g_summary_writer.as_default():
+            tf.summary.scalar('loss', self.g_loss.result(), step=epoch)
+
+        with self.d_summary_writer.as_default():
+            tf.summary.scalar('loss', self.d_loss.result(), step=epoch)
+
+        with self.w_summary_writer.as_default():
+            tf.summary.scalar('loss', self.w_loss.result(), step=epoch)
+
+    def resetting_states(self):
+        self.g_loss.reset_states()
+        self.d_loss.reset_states()
+        self.w_loss.reset_states()
+
+    @tf.function
+    def train_step(self, noise, real):
+        batch_size = len(real)
+
+        for i in range(self.n_critic):
+            with tf.GradientTape() as t:
+                fake = self.generator(noise, training=True)
+                d_real = self.discriminator(real, training=True)
+                d_fake = self.discriminator(fake, training=True)
+                d_loss = self.discriminator_loss(d_real, d_fake)
+
+            new_d_loss = self.update_discriminator_loss(d_loss, batch_size, fake, real)
+            c_grad = t.gradient(new_d_loss, self.discriminator.trainable_variables)
+            self.discriminator_opt.apply_gradients(zip(c_grad, self.discriminator.trainable_variables))
+
+        # train generator
+        with tf.GradientTape() as gen_tape:
+            fake_images = self.generator(noise, training=True)
+            d_fake = self.discriminator(fake_images, training=True)     # training=False?
+            g_loss = self.generator_loss(d_fake)
+
+        gen_grads = gen_tape.gradient(g_loss, self.generator.trainable_variables)
+        self.generator_opt.apply_gradients(zip(gen_grads, self.generator.trainable_variables))
+
+        # for tensorboard
+        self.g_loss(g_loss)
+        self.d_loss(new_d_loss)
+        self.w_loss((-1)*(d_loss))  # wasserstein distance
+
+        return None
+
+    def train(self, batch_size, batches, training_data, input_to_GAN, epochs):
 
 
-                epsilon = tf.random.uniform(shape=[batch_size, 1], minval=0., maxval=1.)
-                #print ('types', real.dtype, epsilon.dtype, fake.dtype)                
-                interpolated = real + epsilon * (fake - real)  
-                t1.watch(interpolated)
-                c_inter = c(interpolated, training=True)  
+        losses = np.zeros((epochs, 4))
 
+        for epoch in range(epochs):
+            noise = input_to_GAN
+            real_data = training_data  # X1.astype('int')
 
-                
-            grad_interpolated = t1.gradient(c_inter, interpolated)
-            slopes = tf.sqrt(tf.reduce_sum(tf.square(grad_interpolated) + 1e-12, axis=[1])) # 
-            gradient_penalty = tf.reduce_mean((slopes - 1.) ** 2)
-            new_d_loss = d_loss + (lmbda*gradient_penalty)  #new c loss
-        
-        c_grad = t.gradient(new_d_loss, c.trainable_variables)
-        c_opt.apply_gradients(zip(c_grad, c.trainable_variables))
-
-
-    ##### train generator #####
-
-    with tf.GradientTape() as gen_tape:
-        fake_images = g(noise, training=True)
-        d_fake = c(fake_images, training=True) # training=False?
-        g_loss = generator_loss(d_fake)
-
-    gen_grads = gen_tape.gradient(g_loss, g.trainable_variables)
-    g_opt.apply_gradients(zip(gen_grads, g.trainable_variables))
-
-
-    ### for tensorboard
-    g1_loss(g_loss)
-    d1_loss(new_d_loss)
-    w_loss((-1)*(d_loss))  #wasserstein distance
-
-
-    return 
-
-
-def train(nsteps,ndims,lmbda,n_critic,batch_size,batches,training_data,input_to_GAN, epochs, g, c, g_opt, c_opt, g1_loss, d1_loss, w_loss, g1_summary_writer, d1_summary_writer, w_summary_writer):
-
-    losses = np.zeros((epochs,4))
-
-    for epoch in range(epochs):
-
-        noise = input_to_GAN
-        real_data = training_data #X1.astype('int')
-
-        # uncommenting this line means that the noise is not paired with the outputs (probably desirable)
-        #noise = np.random.normal(size=[noise.shape[0],noise.shape[1]])
- 
-        real_data, noise = sklearn.utils.shuffle(real_data, noise) #shuffle each epoch
-        
-        xx1 = real_data.reshape(batches, batch_size, ndims*nsteps)
-        inpt1 = noise.reshape(batches, batch_size, ndims)
-        
-        
-        for i in range(len(xx1)):
-            train_step(inpt1[i], xx1[i], lmbda, n_critic, g, c, g_opt, c_opt, g1_loss, d1_loss, w_loss)
-
-        print('epoch:', epoch, '*************************************************')    
-        print('gen loss', g1_loss.result().numpy(), 'd loss', d1_loss.result().numpy(), 'w_loss' , w_loss.result().numpy())
-
-        losses[epoch,:] = [ epoch+1, g1_loss.result().numpy() ,   d1_loss.result().numpy(),  w_loss.result().numpy()]
-
-        with g1_summary_writer.as_default():
-            tf.summary.scalar('loss', g1_loss.result(), step=epoch)
-
-        with d1_summary_writer.as_default():
-            tf.summary.scalar('loss', d1_loss.result(), step=epoch)
-
-        with w_summary_writer.as_default():
-            tf.summary.scalar('loss', w_loss.result(), step=epoch)
+            # uncommenting this line means that the noise is not paired with the outputs (probably desirable)
+            # noise = np.random.normal(size=[noise.shape[0],noise.shape[1]])
+    
+            real_data, noise = sklearn.utils.shuffle(real_data, noise) #shuffle each epoch
             
-        #print('reset states')
-        g1_loss.reset_states()
-        d1_loss.reset_states()
-        w_loss.reset_states()
+            xx1 = real_data.reshape(batches, batch_size, self.ndims*self.nsteps)
+            inpt1 = noise.reshape(batches, batch_size, self.ndims)
 
-        if (epoch + 1) % 1000 == 0:
-        #if epoch < 100 or (epoch + 1) % 100 == 0 :
-                       
-            saved_g1_dir = './saved_g_' + str(epoch + 1)
-            saved_d1_dir = './saved_c_' + str(epoch + 1)
-            tf.keras.models.save_model(g, saved_g1_dir)
-            tf.keras.models.save_model(c, saved_d1_dir)
+            for i in range(batch_size):
+                self.train_step(inpt1[i], xx1[i])
 
-    np.savetxt('losses.csv', losses, delimiter=',')
+            print('epoch:', epoch)    
+            print('gen loss', self.g_loss.result().numpy(), 'd loss', self.d_loss.result().numpy(), 'w_loss' , self.w_loss.result().numpy())
+            losses[epoch,:] = [epoch+1, self.g_loss.result().numpy(), self.d_loss.result().numpy(), self.w_loss.result().numpy()]
 
-    return g
+            self.write_summary(epoch)
+            self.resetting_states()
 
+            if (epoch + 1) % 1000 == 0:
+                self.save_gan(epoch)
 
+        np.savetxt('losses.csv', losses, delimiter=',')
 
+        return None
 
 def learn_hypersurface_from_POD_coeffs(input_to_GAN, training_data, nsteps, nPOD, ndims, lmbda, n_critic, batch_size, batches, ndims_latent_input):
     # nPOD not needed
@@ -229,7 +251,7 @@ def learn_hypersurface_from_POD_coeffs(input_to_GAN, training_data, nsteps, nPOD
 
     print('beginning training')
     epochs = 500
-    generator = train(nsteps,ndims,lmbda,n_critic,batch_size,batches,training_data,input_to_GAN, epochs, g, c, g_opt, c_opt, g1_loss, d1_loss, w_loss, g1_summary_writer, d1_summary_writer, w_summary_writer)
+    generator = train(nsteps,ndims,lmbda,n_critic,batch_size,batches,training_data,input_to_GAN, epochs, g, c, g_opt, c_opt, g1_summary_writer, d1_summary_writer, w_summary_writer)
     print('ending training')
 
 
