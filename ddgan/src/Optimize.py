a7e1132cda40c02a73c28310aac123cda5f23d0e
acse-jat20 <72030772+acse-jat20@users.noreply.github.com>
2021-06-29 17:56:07 +0100

    Update Optimize.py

--- ddgan/src/Optimize.py
+++ ddgan/src/Optimize.py
@@ -29,7 +29,7 @@ class Optimize:
     @tf.function
     def mse_loss(self, input, output):
         if self.bounds is not None:
-            if tf.math.reduce_sum(tf.math.abs(input) > self.bounds) > 0:
+            if tf.math.reduce_sum((tf.math.abs(input) > self.bounds)*1) > 0:
                 return 1.e5
 
         if self.eigenvals is not None:
