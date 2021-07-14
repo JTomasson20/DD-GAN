1233814ce5595b58fcbc6ae64a364b1c97fa0489
acse-jat20 <72030772+acse-jat20@users.noreply.github.com>
2021-07-14 18:47:23 +0100

    Adding V0 of domain decomposition

--- ddgan/src/Optimize.py
+++ ddgan/src/Optimize.py
@@ -24,25 +24,30 @@ class Optimize:
     eigenvals: np.ndarray = None
     bounds: float = 1e5  # set to inf if not in use
 
+    # Only for DD
+    evaluated_subdomains: int = 2
+    cycles: int = 5
+
     mse = tf.keras.losses.MeanSquaredError()
     optimizer = tf.keras.optimizers.Adam(5e-3)
+    nOptimized: int = 90
 
     def mse_loss(self, input, output):
+        # if tf.math.greater(tf.math.reduce_max(tf.math.abs(output)),
+        #  tf.constant(self.bounds)):
+        #     return 1.e5
 
-        if tf.math.reduce_max(tf.math.abs(output)) > self.bounds:
-            return 1.e5
+        # if self.eigenvals is not None:
+        #     print(input.shape)
+        #     print(self.eigenvals.shape)
+        #     return self.mse(
+        #         tf.math.multiply(tf.math.sqrt(self.eigenvals), input),
+        #         tf.math.multiply(tf.math.sqrt(self.eigenvals), output)
+        #             )
+        # else:
+        return self.mse(input, output)
 
-        elif self.eigenvals is not None:
-            print(input.shape)
-            print(self.eigenvals.shape)
-            return self.mse(
-                tf.math.multiply(tf.math.sqrt(self.eigenvals), input),
-                tf.math.multiply(tf.math.sqrt(self.eigenvals), output)
-                    )
-        else:
-            return self.mse(input, output)
-
-    @tf.function
+    # @tf.function
     def opt_latent_var(self, latent_var: tf.Variable, output: np.ndarray):
         """
         Main input optimization loop optimizing the latent variable
@@ -62,7 +67,7 @@ class Optimize:
             r = self.gan.generator(latent_var, training=False)
 
             loss = self.mse_loss(output,
-                                 r[:, :self.gan.ndims*(self.gan.nsteps - 1)]
+                                 r[:, :self.nOptimized]
                                  )
 
         gradients = tape.gradient(loss, latent_var)
@@ -112,7 +117,7 @@ class Optimize:
 
             r = self.gan.generator(ip, training=False)
             loss = self.mse(real_output,
-                            r[:, :self.gan.ndims*(self.gan.nsteps - 1)])
+                            r[:, :self.nOptimized])
 
             ip_np = ip.numpy()
 
@@ -152,6 +157,11 @@ class Optimize:
 
             prediction = self.gan.generator(updated, training=False)
 
+            # Not so sure about the this line but it makes sense to me
+            # that only one step at a time is changed so start by overwriting
+            # the first steps with the previous values
+            prediction[:, :, :self.nOptimized] = the_input
+
             # Last 4 images become next first 4 images
             next_input = prediction[:, self.gan.ndims:]
 
@@ -180,19 +190,210 @@ class Optimize:
             np.ndarray: predictions
         """
         assert self.gan is not None, "Please initialize using an active GAN"
+        self.nOptimized = self.gan.ndims*(self.gan.nsteps - 1)
 
-        inn = training_data[self.start_from,
-                            :(self.gan.nsteps-1)*self.nPOD
-                            ].reshape(1, (self.gan.nsteps - 1) * self.nPOD)
+        inn = training_data[
+            self.start_from,
+            :(self.gan.nsteps-1)*self.nPOD
+            ].reshape(1, self.nOptimized)
 
-        initial_comp = training_data[self.start_from,
-                                     :(self.gan.nsteps-1)*self.nPOD
-                                     ].reshape(self.gan.nsteps - 1,
-                                               self.nPOD
-                                               )
+        initial_comp = training_data[
+            self.start_from,
+            :(self.gan.nsteps-1)*self.nPOD
+            ].reshape(self.gan.nsteps - 1, self.nPOD)
 
         flds = self.timesteps(initial_comp, inn)
         if scaling is not None:
             flds = scaling.inverse_transform(flds).T
 
         return flds
+
+    def timestepsDD(self, initial, inn, boundrary_condition):
+        """
+        Outermost loop. Collecting the predicted points and
+        iterating through predictions
+
+        Args:
+            initial (np.ndarray): Initial value array
+            inn (np.ndarray): Gan input array
+
+        Returns:
+            np.ndarray: Predicted points
+        """
+        # Dynamic predictions. Always same shape
+        the_input = tf.convert_to_tensor(inn)
+
+        # Structured predictions. Data added as we go
+        flds = tf.convert_to_tensor(initial)
+
+        # Indexes for the next iteration
+        indexing = np.concatenate([
+            np.arange(
+                self.nPOD, self.nPOD*self.gan.nsteps*2 + self.nPOD
+                ),
+            np.arange(
+                self.nPOD * self.gan.nsteps * 2 + self.nPOD,  # -1,
+                self.nPOD * self.gan.nsteps * 3
+                )]
+        )
+
+        # Latent space
+        current_latent = tf.Variable(tf.zeros([
+            self.evaluated_subdomains,
+            1,
+            self.nLatent
+            ]))
+
+        # updated = np.zeros((self.evaluated_subdomains, 1, self.nLatent))
+        updated = current_latent
+
+        prediction = np.zeros(
+            (self.evaluated_subdomains,
+             1,
+             self.nLatent * self.nPOD * 3))
+
+        # Moving back and forward to be iterated through
+        # 2 domains become (0,1)
+        # 3 domains become (0,1,2,1)
+        domains = np.concatenate((
+            np.arange(self.evaluated_subdomains),
+            np.arange(self.evaluated_subdomains)[1:-1][::-1]
+        ))
+
+        for i in range(self.npredictions):
+            print('Time step: \t', i)
+
+            # Input the correct data to left boundrary
+            tf.tensor_scatter_nd_update(
+                the_input,
+                np.array(
+                    [np.zeros(self.nPOD),
+                     np.zeros(self.nPOD),
+                     np.arange(self.nPOD*(self.gan.nsteps - 1),
+                               self.nPOD*self.gan.nsteps)],
+                    dtype=np.int32).T,
+                boundrary_condition[
+                    0, i + self.start_from])
+
+            # Input data to right boundrary
+            tf.tensor_scatter_nd_update(
+                the_input,
+                np.array(
+                    [np.ones(self.nPOD) * (len(the_input)-1),
+                     np.zeros(self.nPOD),
+                     np.arange(self.nPOD*(2*self.gan.nsteps - 1),
+                               self.nPOD*2*self.gan.nsteps)],
+                    dtype=np.int32).T,
+                boundrary_condition[
+                    1, i + self.start_from])
+
+            # Do a predict/update cycle
+            for _ in range(self.cycles):
+                for j in domains:
+                    tmp, __, __, __, __ = \
+                        self.timestep_loop(
+                            the_input[j],
+                            tf.Variable(current_latent[j]))
+
+                    # Essentially updated[j] = tmp
+                    tf.tensor_scatter_nd_update(
+                        updated,
+                        np.array([
+                            np.ones(self.nLatent)*j,
+                            np.zeros(self.nLatent),
+                            np.arange(self.nLatent)],
+                         dtype=np.int32).T,
+                        tmp[0])
+
+                    # Update the next prediction
+                    prediction[j, 0] = self.gan.generator(
+                        updated[j],
+                        training=False)
+
+                    # Communicate the update to neighbours
+                    # everybody except leftmost updates to the left
+                    if j != 0:
+                        tf.tensor_scatter_nd_update(
+                            the_input,
+                            np.array(
+                                [np.ones(self.nPOD) * (j-1),
+                                 np.zeros(self.nPOD),
+                                 np.arange(self.nPOD*(2*self.gan.nsteps - 1),
+                                           self.nPOD*2*self.gan.nsteps)],
+                                dtype=np.int32).T,
+                            prediction[j, 0, -self.nPOD:])
+
+                    # everybody except rightmost updates to the right
+                    if j+1 != self.evaluated_subdomains:
+                        tf.tensor_scatter_nd_update(
+                            the_input,
+                            np.array(
+                                [np.ones(self.nPOD) * (j+1),
+                                 np.zeros(self.nPOD),
+                                 np.arange(self.nPOD*(self.gan.nsteps - 1),
+                                           self.nPOD*self.gan.nsteps)],
+                                dtype=np.int32).T,
+                            prediction[j, 0, -self.nPOD:])
+
+                    current_latent = updated
+
+                # Redo the first one to complete the final cycle
+                tmp, __, __, __, __ = \
+                    self.timestep_loop(
+                        the_input[0],
+                        tf.Variable(current_latent[0]))
+
+                # updated[j] = tmp
+                tf.tensor_scatter_nd_update(
+                    updated,
+                    np.array([
+                        np.zeros(self.nLatent),
+                        np.zeros(self.nLatent),
+                        np.arange(self.nLatent)],
+                        dtype=np.int32).T,
+                    tmp[0])
+
+                prediction[0, 0] = self.gan.generator(
+                    updated[0], training=False)
+
+                current_latent = updated
+
+            # Last 4 images become next first 4 images
+            # next_input = prediction[:, :, indexing]
+            prediction[:, :, :self.nOptimized] = the_input
+            next_input = prediction[:, :, indexing]
+            print(the_input)
+            # Last image out of 5 is added to list of compressed vars
+            new_result = prediction[:, :, self.nOptimized:]
+            flds = tf.concat([flds, new_result], 1)
+            # print(flds)
+            the_input = next_input  # .numpy()
+
+        return flds
+
+    def predictDD(self, training_data, boundrary_conditions):
+        """
+        Communicator with optimizaiton scripts.
+        Breaks up training data and feeds it forward.
+
+        Args:
+            training_data (np.ndarray)
+        """
+        # Updating these values only if DD is used
+        self.evaluated_subdomains = len(training_data)
+        self.nOptimized = (self.gan.nsteps*3 - 1) * self.nPOD
+
+        inn = []  # Flattened version of data
+        initial_comp = []  # Structured version of data
+
+        for i in range(self.evaluated_subdomains):
+            inn.append(training_data[
+                i, self.start_from, :self.nOptimized
+                ].reshape(1, self.nOptimized))
+
+            initial_comp.append(training_data[
+                i, self.start_from, :self.nOptimized
+                ].reshape(self.gan.nsteps*3 - 1, self.nPOD))
+
+        return self.timestepsDD(initial_comp, inn, boundrary_conditions)
+
